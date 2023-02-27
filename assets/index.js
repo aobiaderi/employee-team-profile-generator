df619a814669d5e020065d421993b235dca8b83a
Obee123 <aobiaderi@yahoo.com>
2023-02-27 21:26:19 +0000

    destructured manager,intern and engineer objects and created function to prompt user for manager information

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const {Manager} = require("./lib/Manager");
+const {Engineer} = require("./lib/Engineer");
+const {Intern} = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -13,3 +13,39 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const employees = [];
+
+// Function to prompt user for manager's information
+function promptManager() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the team manager's name?",
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the team manager's employee ID?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the team manager's email address?",
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is the team manager's office number?",
+    },
+
+]).then(function(answer) {
+    let manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber)
+
+    employees.push(manager);
+      // Call promptNextAction function to continue the program
+    promptNextAction(); 
+
+    })
+}; // end of function 
+promptManager(); // Call promptManager function to start the program
